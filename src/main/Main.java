3a286851e934fb8ef6797ba94af601324b33628d
selfstudymaster <webselfmaster@gmail.com>
2021-03-05 17:05:50 +0900

    コーディング練習

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,21 @@
 package main;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		System.out.println("Hello tensai!");
+//	}
+//
+//}
+
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("Hello");
+//	}
+//}
 
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println("Hello tensai!");
+		System.out.println("Thanks!");
 	}
-
 }
