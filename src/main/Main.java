7d8cb9f1b7f444a3fed0064b68343f8f2e7e1594
selfstudymaster <webselfmaster@gmail.com>
2021-03-05 17:29:49 +0900

    list01-05完了

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
-//
+
+// コーディング練習用
 //public class Main {
 //
 //	public static void main(String[] args) {
@@ -8,14 +9,20 @@ package main;
 //
 //}
 
+// list01-05
 //public class Main {
 //	public static void main(String[] args) {
-//		System.out.println("Hello");
+//		final double TAX = 1.08;
+//		int fax = 5;
+//		System.out.println( "5万円から4万円に値下げします");
+//		fax = 4;
+//		System.out.println( "ＦＡＸの新価格（税込み）");
+//		System.out.println( fax * TAX + "万円" );
 //	}
 //}
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Thanks!");
+		System.out.println("Hello, I'm Mozart of Programming World!");
 	}
 }
